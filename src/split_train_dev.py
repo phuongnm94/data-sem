a3e7ec16981e6d046ea0409eabbe479309c46670
Nguyen Minh Phuong <phuongnm.bkhn@gmail.com>
2019-12-15 17:29:01 +0900

    update split holdout train-dev-test for geo

---
+++ src/split_train_dev.py
@@ -0,0 +1,29 @@
+import torch
+
+from onmt.utils import set_random_seed
+import pandas as pd
+import numpy as np
+
+if __name__=="__main__":
+    set_random_seed(100, torch.cuda.is_available())
+
+    path_file_train = "../geo/org/train.txt"
+    path_file_test = "../geo/org/test.txt"
+    data = pd.read_csv(path_file_train, delimiter="\t", header=None)
+    test_set = pd.read_csv(path_file_test, delimiter="\t", header=None)
+    idx_random = np.random.permutation(len(data))
+
+    test_idx_choices = idx_random[:int(0.1*len(data))]
+    train_idx_choices = [i for i in range(len(data)) if i not in test_idx_choices]
+
+    dev_set = data.iloc[test_idx_choices, :]
+    train_set = data.iloc[train_idx_choices, :]
+
+    folder_out = "../geo/train-dev-test/"
+    dev_set.iloc[:, 0].to_csv(folder_out + "X_dev_5.tsv", header=False, index=None)
+    dev_set.iloc[:, 1].to_csv(folder_out + "Y_dev_5.tsv", header=False, index=None)
+    train_set.iloc[:, 0].to_csv(folder_out + "X_train_5.tsv", header=False, index=None)
+    train_set.iloc[:, 1].to_csv(folder_out + "Y_train_5.tsv", header=False, index=None)
+    test_set.iloc[:, 0].to_csv(folder_out + "X_test_5.tsv", header=False, index=None)
+    test_set.iloc[:, 1].to_csv(folder_out + "Y_test_5.tsv", header=False, index=None)
+
