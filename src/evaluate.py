9c93070aa6a1ff6f9c40155d932df7e6dc39ab98
Nguyen Minh Phuong <phuongnm.bkhn@gmail.com>
2020-11-16 01:37:41 +0900

    add arg namefile pred/src/tgt

--- src/evaluate.py
+++ src/evaluate.py
@@ -16,6 +16,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Evaluate accuracy of logical form.')
     parser.add_argument('--path',
                         default="../data/geo/8536_seq2seq_marking_10000step", )
+    parser.add_argument('--src',
+                        default="X_test_5.tsv", )
+    parser.add_argument('--tgt',
+                        default="Y_test_5.tsv", )
+    parser.add_argument('--pred',
+                        default="Y_pred_5.tsv", )
     parser.add_argument('--type', required=False,
                         default="logic",
                         help="Choose type to run method compare in [logic|code]")
@@ -34,18 +40,18 @@ if __name__ == "__main__":
         obj_parser = SCode
 
     data = {}
-    for file_name in ["X_test_5.tsv", "Y_test_5.tsv", "Y_pred_5.tsv"]:
+    for file_name in [args.src, args.tgt, args.pred]:
         with open("{}/{}".format(args.path, file_name), "rt", encoding="utf8") as f:
             lines = [l.strip() for l in f.readlines()]
             data[file_name] = lines
-    count_all = len(data['X_test_5.tsv'])
+    count_all = len(data[args.src])
 
     count_logic = 0
     count_exact_matching = 0
     count_nbest = 0
-    for i, logic1 in enumerate(data["Y_test_5.tsv"]):
+    for i, logic1 in enumerate(data[args.tgt]):
         for j in range(args.n_best):
-            logic2 = data["Y_pred_5.tsv"][i * args.n_best + j]
+            logic2 = data[args.pred][i * args.n_best + j]
             if j == 0:
                 try:
                     if logic2 == logic1:
@@ -59,7 +65,7 @@ if __name__ == "__main__":
                         break
                     else:
                         log("-- sentence {}, logic/code gold != logic/code pred --".format(i))
-                        log(data["X_test_5.tsv"][i])
+                        log(data[args.src][i])
                         log(logic1)
                         log(logic2)
                 except:
@@ -68,7 +74,7 @@ if __name__ == "__main__":
                 if (logic1 == logic2 or (
                         method_eq(obj_parser(logic1), obj_parser(logic2), not_layout=True))):
                     log("!! sentence {}, logic/code gold == logic/code pred {} !!".format(i, j + 1))
-                    log(data["X_test_5.tsv"][i])
+                    log(data[args.src][i])
                     log(logic1)
                     log(logic2)
                     count_nbest += 1.0
